0fa2e42934ebc34eee569db9e2bcd3a476d3fa4b
LiamMaclean216 <lmaclean234@gmail.com>
2026-05-28 14:47:57 -0400

    oopss

--- build_prod.py
+++ build_prod.py
@@ -8,6 +8,5 @@ if __name__ == "__main__":
         alias="stepscale-sandbox",
         cpu_count=2,
         memory_mb=4096,  # 4 GB RAM
-        on_build_logs=default_build_logger(),
-        api_key="e2b_443ad1b90bbaa3c92096f9eab9e0900f3012f8b3"
+        on_build_logs=default_build_logger()
     )
